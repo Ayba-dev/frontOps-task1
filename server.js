98749c15a4c1b8e7b46f647b2aa5ea2f118484d8
Aibek <aibekmo1998@gmail.com>
2024-11-29 01:05:00 +0600

    added docker-compose

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const express =  require('express')
-const  app = express();
+const express = require('express');
+
+const app = express();
 
 
-app.get((req, res) => {
-    res.send('hello')
+
+app.get('/', (req, res) => {
+    fetch("http://localhost:3005")
+        .then(res => res.json())
+        .then(date => {
+            res.send(`hello current ${new Date()}`)
+        })
 });
 
-app.listen(3000,() => {
-    console.log(`Server is running`);
+
+app.listen(3000, () => {
+    console.log('ready')
 })
+
+
+
+
+
+
+
+
